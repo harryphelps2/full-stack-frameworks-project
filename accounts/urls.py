134dcacc1869c869be11530b1520c81529b1835e
harryphelps2 <harryphelps1@gmail.com>
2018-09-05 08:42:58 +0100

    Develop password reset functionality to send email to console

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,14 @@
-"""
-Here we want to include URLS for the index.html, profile and login page
-for index.html we want to got a url of acccounts/ to render the index.html page
-for /login we want to go to the login page and for registration we want to go to the 
-registration.html
-
-step 1 import the url module
-we need to import the views that take us to where we need to go
-"""
-from django.urls import path
+from django.urls import path, include, re_path
+from . import urls_reset
 from .views import user_login, user_profile
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, include, reverse_lazy
 
 urlpatterns = [
     path('login/', user_login, name='login'),
-    path('profile/', user_profile, name='profile')
+    path('profile/', user_profile, name='profile'),
+    path('password_reset/', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name="password_reset_complete"), 
 ]
