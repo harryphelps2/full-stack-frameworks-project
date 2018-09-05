97042d4a6eceeb881b7e188332a339a14a4b72a3
harryphelps2 <harryphelps1@gmail.com>
2018-09-05 08:47:37 +0100

    Configure email server to send real emails

--- artproject/settings.py
+++ artproject/settings.py
@@ -126,4 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
