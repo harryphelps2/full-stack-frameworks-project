c04a1f5858559e59f01aac0933c8c299ba8d363f
harryphelps2 <harryphelps1@gmail.com>
2018-09-30 08:23:17 +0100

    Customer approval of proposed commission functionality and routing in commission status function.

--- commission/urls.py
+++ commission/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, re_path
-from .views import commission, request_submitted, awaiting_approval
+from .views import commission_status, customer_submit_request, request_submitted, awaiting_approval, accept_proposal, awaiting_deposit, work_in_progress
 
 urlpatterns = [
-    path('', commission, name='commission'),
-    path('submitted', request_submitted, name='request_submitted'),
+    path('', commission_status, name='commission_status'),
+    path('customer_submit_request', customer_submit_request, name='customer_submit_request'),
+    path('request_submitted', request_submitted, name='request_submitted'),
     path('awaiting_approval', awaiting_approval, name='awaiting_approval'),
+    path('accept_proposal', accept_proposal, name='accept_proposal'),
+    path('awaiting_deposit', awaiting_deposit, name='awaiting_deposit'),
+    path('work_in_progress', work_in_progress, name='work_in_progress'),
 ]
--- commission/views.py
+++ commission/views.py
@@ -7,12 +7,37 @@ from .models import Commission
 
 
 @login_required
-def commission(request):
+def commission_status(request):
     """
     Commission form, customer fills in details for request and submits.
     Creates new entry for inspection and acceptance by artist.
+    Redirect to proposal submitted if already have a commission.
+    else if accepted = false redirect to awaiting approval
+    else if accepted = true redirect to pay deposit
+    else if deposit_paid = true redirect to work in progress
+    Or work in progress if needed
     """
     # add logic so that if there is already a commission in progress it doesn't let you see this page
+    user = request.user
+    #need to convert this into an object I think instead of query set
+    try:
+        current_commission = Commission.objects.get(user=user)
+        if not current_commission.price:
+            return redirect(reverse('request_submitted'))
+        elif not current_commission.price:
+            return redirect(reverse('awaiting_approval'))
+        else: 
+            return redirect(reverse('awaiting_deposit'))
+    except:
+        commission_proposal_form = CommissionRequestForm()
+        return render (request, 'submit_proposal.html', {'commission_proposal_form':commission_proposal_form})
+        # # if not current_commission.accepted:
+        # return redirect(reverse('request_submitted'))
+        
+    
+
+@login_required
+def customer_submit_request(request):
     if request.method=='POST':
         commission_proposal_form = CommissionRequestForm(request.POST)
         if commission_proposal_form.is_valid():
@@ -20,26 +45,69 @@ def commission(request):
             commission_proposal.proposed_date =  timezone.now()
             commission_proposal.user = request.user
             commission_proposal.save()
-            return redirect(reverse('awaiting_acceptance'))
+            return redirect(reverse('request_submitted'))
     else:
         commission_proposal_form = CommissionRequestForm()
     return render (request, 'submit_proposal.html', {'commission_proposal_form':commission_proposal_form})
+        
+
 
 @login_required
 def request_submitted(request):
     """
     Shows view of proposal and waiting meme. Artist can though then upload
     image of small study and price so customer can look at decide whether 
-    to accept or not. 
+    to accept or not. Artist then updates the price on django admin. 
     """
     user = request.user
-    request_submitted = Commission.objects.filter(accepted_date=None, price=None, user=user)
-    return render(request, 'request_submitted.html', {'request_submitted':request_submitted})
+    request_submitted = get_object_or_404(Commission, user=user)
+    if not request_submitted.price:
+        return render(request, 'request_submitted.html', {'request_submitted':request_submitted})
+    else:
+        return redirect(reverse('awaiting_approval'))    
 
 @login_required
 def awaiting_approval(request):
     """
-    Shows price and study picture from artist for customer approval.
+    Shows price and study picture from artist for customer approval. Then customer needs to approve.
     """
-    awaiting_approval = Commission.objects.filter(accepted_date=None)
+    user = request.user
+    awaiting_approval = get_object_or_404(Commission, user=user)
     return render(request, 'awaiting_approval.html', {'awaiting_approval':awaiting_approval})
+
+@login_required
+def accept_proposal(request):
+    """
+    View to update database when proposal is accepted.
+    Add 10%of price to cart and redirect to cart.
+    """
+    user = request.user
+    if request.method == 'POST':
+        print(request.POST)
+        accepted_commission = get_object_or_404(Commission, user=user)
+        print(accepted_commission)
+        accepted_commission.accepted = True
+        accepted_commission.accepted_date = timezone.now()
+        accepted_commission.save()
+        """
+        Redirect here to add deposit to cart as half of the price.
+
+        """
+        return redirect(reverse('awaiting_deposit'))
+
+@login_required
+def awaiting_deposit(request):
+    """
+    Page to pay the deposit.
+    """
+    
+    return render(request, 'awaiting_deposit.html', {'awaiting_deposit':awaiting_deposit})
+
+@login_required
+def work_in_progress(request):
+    """
+    View to show the customer how the piece is coming along
+    """
+    return render(request, 'work_in_progress.html')
+                    
+
